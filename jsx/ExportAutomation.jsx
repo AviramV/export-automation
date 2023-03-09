85428d947d07d9a0a77e411a32b2d433996bdd63
AviramV <aviramv2@gmail.com>
2023-03-09 23:13:57 +0200

    V1.0.1: Fix wrong encoding level in H.264 preset

--- jsx/ExportAutomation.jsx
+++ jsx/ExportAutomation.jsx
@@ -4,7 +4,6 @@ function automation(selectedFolder, preset) {
     var sequence = project.activeSequence;
     var projectFolder = project.path.slice(0, project.path.lastIndexOf('/'));
     var exportLocation = selectedFolder || projectFolder // user selected. defaults to project folder
-    // var preset = "/Users/aviramv/Documents/Adobe/Adobe Media Encoder/23.0/Presets/EDX 101, FHD ,mp4, 50Mbps.epr"
     
     app.encoder.launchEncoder();
     
